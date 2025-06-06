bf11cd4ca32a07b3e34c2216ee662b4a106b0d4e
Artyom Shendik <samok99@bk.ru>
2025-06-06 21:06:55 +0300

    feat: added Cart

---
+++ app/modules/Cart/Cart.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { CartDrawer } from './components/CartDrawer/CartDrawer';
+
+export const Cart: FC = () => {
+  return (
+    <>
+      <CartDrawer />
+    </>
+  );
+};
