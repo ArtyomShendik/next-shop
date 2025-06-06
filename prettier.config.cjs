3ea0ccbdb61d6b0483679bd5e14e4903b2cd755e
Artyom Shendik <samok99@bk.ru>
2025-06-06 21:06:21 +0300

    feat: added prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  printWidth: 80,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+};
